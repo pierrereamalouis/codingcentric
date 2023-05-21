e59b518c6be0e3919692de2cb8bc24bf541219da
Pierre <pierrereama.louis@gmail.com>
2023-05-20 23:08:47 -0400

    add tailwindcss postcss auotprefixer prettier to project

--- content/index.md
+++ content/index.md
@@ -7,3 +7,9 @@ export default {
   awesome: true,
 };
 ```
+
+Hello, this is a [Markdown]{.px-1 .bg-blue-200 .rounded-lg} file!
+
+::div{.w-full .bg-green-200 .p-4}
+Another way to use it!
+::
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/content"],
+  modules: ["@nuxt/content", "@nuxtjs/tailwindcss"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 });
